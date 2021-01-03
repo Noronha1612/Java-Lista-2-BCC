a8318ac6a9995737c308e701199a779fa752fd2a
Gabriel Noronha Cavalcante Pessoa <56375499+Noronha1612@users.noreply.github.com>
2021-01-03 04:20:22 -0300

    Ex03 - LicencaBasica class included

---
+++ src/questao03/LicencaBasica.java
@@ -0,0 +1,28 @@
+package questao03;
+
+import java.time.LocalDateTime;
+
+public class LicencaBasica extends Receita {
+
+    int totalContas;
+
+    public LicencaBasica(LocalDateTime data, String descricao, int totalContas) {
+        super(data, descricao);
+        setTotalContas(totalContas);
+    }
+
+    @Override
+    public String calcularTotal() {
+        final double BASIC_PRICE = 20;
+
+        return Double.toString(BASIC_PRICE * totalContas);
+    }
+
+    public int getTotalContas() {
+        return totalContas;
+    }
+
+    public void setTotalContas(int totalContas) {
+        this.totalContas = totalContas;
+    }
+}
