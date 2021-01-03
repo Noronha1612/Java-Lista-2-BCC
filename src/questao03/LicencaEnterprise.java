dd9512a730f9c9d5438f2ab788f427b7864fd01b
Gabriel Noronha Cavalcante Pessoa <56375499+Noronha1612@users.noreply.github.com>
2021-01-03 04:21:38 -0300

    Ex03 - LicencaEnterprise class included

---
+++ src/questao03/LicencaEnterprise.java
@@ -0,0 +1,28 @@
+package questao03;
+
+import java.time.LocalDateTime;
+
+public class LicencaEnterprise extends Receita {
+
+    int totalContas;
+
+    public LicencaEnterprise(LocalDateTime data, String descricao, int totalContas) {
+        super(data, descricao);
+        setTotalContas(totalContas);
+    }
+
+    @Override
+    public String calcularTotal() {
+        final double ENTERPRISE_PRICE = 150;
+
+        return Double.toString(ENTERPRISE_PRICE * totalContas);
+    }
+
+    public int getTotalContas() {
+        return totalContas;
+    }
+
+    public void setTotalContas(int totalContas) {
+        this.totalContas = totalContas;
+    }
+}
