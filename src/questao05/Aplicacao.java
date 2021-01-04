27922749a681c4db90630027fbae3389c02f2736
NoronhaGCP <inc.691@gmail.com>
2021-01-04 13:59:14 -0300

    Ex05 - Basic JavaFX structure created

---
+++ src/questao05/Aplicacao.java
@@ -0,0 +1,14 @@
+package questao05;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Aplicacao implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Todo
+    }
+}
