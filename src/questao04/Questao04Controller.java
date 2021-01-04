f013fc76e6d3d96ec44065124f04aba9eeab7aea
NoronhaGCP <inc.691@gmail.com>
2021-01-04 13:48:57 -0300

    Ex04 - Add handleButtonClick and linked ids to their respective FXML tags

--- src/questao04/Questao04Controller.java
+++ src/questao04/Questao04Controller.java
@@ -5,21 +5,28 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Questao04Controller implements Initializable {
 
-    @FXML
-    private Button buttonText;
-
-    @FXML
-    private Label labelText;
+    @FXML private Button countButton;
+    @FXML private TextArea textArea;
+    @FXML private Label labelToShow;
 
     @FXML
     private void handleButtonClick(ActionEvent event) {
-        labelText.setText("Clicou!");
+        int countChar = 0;
+
+        String textTyped = textArea.getText();
+
+        for ( int cIndex = 0; cIndex < textTyped.trim().length(); cIndex++ ) {
+            if ( textTyped.trim().charAt(cIndex) != ' ' ) countChar++;
+        }
+
+        labelToShow.setText(String.format("O texto tem %d characteres", countChar));
     }
 
     @Override
