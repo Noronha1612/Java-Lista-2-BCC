2dee104409d174d3ff3a245caa25b138fdf753bf
NoronhaGCP <inc.691@gmail.com>
2021-01-04 14:20:36 -0300

    Ex05 - Linked fxml with controller

--- src/questao05/Aplicacao.java
+++ src/questao05/Aplicacao.java
@@ -1,14 +1,28 @@
 package questao05;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Aplicacao extends Application {
+
+    @FXML private TextField descricao;
+    @FXML private TextField valor;
+    @FXML private TextField quantidade;
+    @FXML private Button calcularBotao;
+
+    @FXML
+    public void handleCalcularBotao(ActionEvent event) {
+        
+    }
 
     @Override
     public void start(Stage stage) throws IOException {
